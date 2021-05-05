035b6510d2bd4fdbf7011a50320023de8c1bad2d
Aleksey Voko <voko.aleksey@gmail.com>
2021-05-05 14:30:46 +0400

    Added mixer package

---
+++ mixer/Mixer.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from utils import get_string_list_from_file, read_src_bs, save_bs_dicts_to_txt
+
+
+def add_groups_to_bs():
+    definitions = 'definitions.txt'
+
+    print('*' * 3)
+    print('Настройки:')
+    print(f'"{definitions}"\n')
+    in_bs, add_groups, out_bs = get_string_list_from_file(
+        definitions, encoding='cp1251')
+
+    print('*' * 3)
+    print('Словарь:')
+    print(f'"{in_bs}"\n')
+    word_forms_bases = list(read_src_bs(in_bs))
+
+    print('Группы:')
+    print(f'"{add_groups}"\n')
+    verbs = list(read_src_bs(add_groups))
+
+    print('... сортировка ...\n')
+    word_forms_bases += verbs
+    save_bs_dicts_to_txt(sorted(word_forms_bases), out_bs)
+
+    print('*' * 3)
+    print(f'В словарь добавлено {len(verbs)} групп словоформ\n')
+
+    print('*' * 3)
+    print(f'Файл с результатами "{out_bs}" сохранён в текущей директории:')
+    print(Path().resolve())
+    print()
+
+    print('*' * 3)
+    print('Для выхода нажмите любую клавишу')
+    input()
+
+
+if __name__ == '__main__':
+    add_groups_to_bs()
+
+# compilation
+# pyinstaller -F mixer/Mixer.py
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import csv
 from pathlib import Path
 
+from word_form import TitleWordForm, WordForm, GroupWordForm
+
 
 def get_string_list_from_file(f_name, encoding='utf-8'):
     with open(Path(f_name), encoding=encoding) as f_in:
@@ -20,3 +22,41 @@ def save_bs_dicts_to_txt(in_dicts: list, f_name, encoding='cp1251'):
     Path(f_name).parent.mkdir(parents=True, exist_ok=True)
     with open(Path(f_name), 'w', encoding=encoding) as f_out:
         f_out.write('\n\n'.join(str(x) for x in in_dicts) + '\n')
+
+
+def get_bs_title_word_form(src_title_word_form):
+    if '.*' in src_title_word_form:
+        src_title_word_form_w_note, src_note = [
+            x.strip() for x in src_title_word_form.split(' .* ')]
+        note = ' '.join(['.*', src_note])
+    else:
+        src_title_word_form_w_note = src_title_word_form
+        note = ''
+    name, *idf_info, = src_title_word_form_w_note.split()
+    if idf_info:
+        idf, *info = idf_info
+    else:
+        idf, *info = '', ''
+    return TitleWordForm(name, idf, info, note)
+
+
+def read_src_bs(f_name: str, encoding='cp1251'):
+    with open(f_name, encoding=encoding) as f_in:
+        src_group_word_form_list = (
+            x.strip() for x in f_in.read().split('\n\n'))
+
+        for src_group_word_form in src_group_word_form_list:
+            src_title_word_form, *src_word_forms = src_group_word_form.split('\n')
+
+            title_word_form = get_bs_title_word_form(src_title_word_form)
+
+            # word_forms
+            word_forms = []
+            for src_word_form in src_word_forms:
+                name, *idf_list, = src_word_form.split()
+                word_forms.append(WordForm(name, ' '.join(idf_list)))
+
+            # group_word_form
+            group_word_form = GroupWordForm(title_word_form, word_forms)
+
+            yield group_word_form
