d46ecc75152fb64e038fd8afe390fed7588ff418
Aleksey Voko <voko.aleksey@gmail.com>
2021-05-06 16:04:28 +0400

    Added verbs passive_past_participle ППС2&2* - ППС3&6ж

--- verbs/passive_past_participle.py
+++ verbs/passive_past_participle.py
@@ -24,6 +24,10 @@ def get_passive_past_participle(src_dict) -> list:
         'ППС8': passive_past_participle_pps8,
         'ППС9': passive_past_participle_pps9,
         'ППС10': passive_past_participle_pps10,
+        'ППС2&2*': passive_past_participle_pps2_2_prim,
+        'ППС2&2о': passive_past_participle_pps2_2o,
+        'ППС2&5ж*': passive_past_participle_pps2_5zh_prim,
+        'ППС3&6ж': passive_past_participle_pps3_6zh,
     }
     return passive_past_participle_tmpl[src_dict['Inf_10']](src_dict)
 
@@ -741,3 +745,295 @@ def passive_past_participle_pps10(src_dict) -> list:
         WordForm(f'{ppsmi[:-2]}о', '.ППСКс'),
         WordForm(f'{ppsmi[:-2]}ы', '.ППСКмн'),
     ]
+
+    return word_forms
+
+
+# ППС2&2*
+def passive_past_participle_pps2_2_prim(src_dict) -> list:
+    gnb1e = list(filter(
+        lambda x: x.idf == '.ГНБ1е',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    pps1mi = f'{gnb1e[:-1]}енный'
+    pps2mi = f'{gnb1e[:-2]}о{gnb1e[2:-1]}енный'
+    word_forms = [
+        WordForm(pps1mi, '.ППС1мИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1мР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1мД'),
+        WordForm(pps1mi, '.ППС1мВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1мП'),
+        WordForm(f'{pps1mi[:-2]}ая', '.ППС1жИ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жР'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жД'),
+        WordForm(f'{pps1mi[:-2]}ую', '.ППС1жВ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жТ1'),
+        WordForm(f'{pps1mi[:-2]}ою', '.ППС1жТ2'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жП'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1сР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1сД'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1сТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1сП'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнИ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнР'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мнД'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнВ'),
+        WordForm(f'{pps1mi[:-2]}ыми', '.ППС1мнТ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнП'),
+        WordForm(f'{pps1mi[:-3]}', '.ППСКм1'),
+        WordForm(f'{pps1mi[:-3]}а', '.ППСКж1'),
+        WordForm(f'{pps1mi[:-3]}о', '.ППСКс1'),
+        WordForm(f'{pps1mi[:-3]}ы', '.ППСКмн1'),
+
+        WordForm(pps2mi, '.ППС2мИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2мР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2мД'),
+        WordForm(pps2mi, '.ППС2мВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2мП'),
+        WordForm(f'{pps2mi[:-2]}ая', '.ППС2жИ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жР'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жД'),
+        WordForm(f'{pps2mi[:-2]}ую', '.ППС2жВ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жТ1'),
+        WordForm(f'{pps2mi[:-2]}ою', '.ППС2жТ2'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жП'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2сР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2сД'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2сТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2сП'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнИ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнР'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мнД'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнВ'),
+        WordForm(f'{pps2mi[:-2]}ыми', '.ППС2мнТ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнП'),
+        WordForm(f'{pps2mi[:-3]}', '.ППСКм2'),
+        WordForm(f'{pps2mi[:-3]}а', '.ППСКж2'),
+        WordForm(f'{pps2mi[:-3]}о', '.ППСКс2'),
+        WordForm(f'{pps2mi[:-3]}ы', '.ППСКмн2'),
+    ]
+
+    return word_forms
+
+
+# ППС2&2о
+def passive_past_participle_pps2_2o(src_dict) -> list:
+    gnb1e = list(filter(
+        lambda x: x.idf == '.ГНБ1е',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    pps1mi = f'{gnb1e[:-1]}енный'
+    pps2mi = f'{gnb1e[:-3]}ощенный'
+    word_forms = [
+        WordForm(pps1mi, '.ППС1мИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1мР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1мД'),
+        WordForm(pps1mi, '.ППС1мВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1мП'),
+        WordForm(f'{pps1mi[:-2]}ая', '.ППС1жИ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жР'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жД'),
+        WordForm(f'{pps1mi[:-2]}ую', '.ППС1жВ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жТ1'),
+        WordForm(f'{pps1mi[:-2]}ою', '.ППС1жТ2'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жП'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1сР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1сД'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1сТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1сП'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнИ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнР'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мнД'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнВ'),
+        WordForm(f'{pps1mi[:-2]}ыми', '.ППС1мнТ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнП'),
+        WordForm(f'{pps1mi[:-3]}', '.ППСКм1'),
+        WordForm(f'{pps1mi[:-3]}а', '.ППСКж1'),
+        WordForm(f'{pps1mi[:-3]}о', '.ППСКс1'),
+        WordForm(f'{pps1mi[:-3]}ы', '.ППСКмн1'),
+
+        WordForm(pps2mi, '.ППС2мИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2мР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2мД'),
+        WordForm(pps2mi, '.ППС2мВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2мП'),
+        WordForm(f'{pps2mi[:-2]}ая', '.ППС2жИ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жР'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жД'),
+        WordForm(f'{pps2mi[:-2]}ую', '.ППС2жВ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жТ1'),
+        WordForm(f'{pps2mi[:-2]}ою', '.ППС2жТ2'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жП'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2сР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2сД'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2сТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2сП'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнИ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнР'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мнД'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнВ'),
+        WordForm(f'{pps2mi[:-2]}ыми', '.ППС2мнТ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнП'),
+        WordForm(f'{pps2mi[:-3]}', '.ППСКм2'),
+        WordForm(f'{pps2mi[:-3]}а', '.ППСКж2'),
+        WordForm(f'{pps2mi[:-3]}о', '.ППСКс2'),
+        WordForm(f'{pps2mi[:-3]}ы', '.ППСКмн2'),
+    ]
+
+    return word_forms
+
+
+# ППС2&5ж*
+def passive_past_participle_pps2_5zh_prim(src_dict) -> list:
+    name = src_dict['name']
+    gnb1e = list(filter(
+        lambda x: x.idf == '.ГНБ1е',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    pps1mi = f'{gnb1e[:-1]}енный'
+    pps2mi = f'{name[:-4]}жденный'
+    word_forms = [
+        WordForm(pps1mi, '.ППС1мИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1мР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1мД'),
+        WordForm(pps1mi, '.ППС1мВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1мП'),
+        WordForm(f'{pps1mi[:-2]}ая', '.ППС1жИ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жР'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жД'),
+        WordForm(f'{pps1mi[:-2]}ую', '.ППС1жВ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жТ1'),
+        WordForm(f'{pps1mi[:-2]}ою', '.ППС1жТ2'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жП'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1сР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1сД'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1сТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1сП'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнИ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнР'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мнД'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнВ'),
+        WordForm(f'{pps1mi[:-2]}ыми', '.ППС1мнТ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнП'),
+        WordForm(f'{pps1mi[:-3]}', '.ППСКм1'),
+        WordForm(f'{pps1mi[:-3]}а', '.ППСКж1'),
+        WordForm(f'{pps1mi[:-3]}о', '.ППСКс1'),
+        WordForm(f'{pps1mi[:-3]}ы', '.ППСКмн1'),
+
+        WordForm(pps2mi, '.ППС2мИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2мР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2мД'),
+        WordForm(pps2mi, '.ППС2мВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2мП'),
+        WordForm(f'{pps2mi[:-2]}ая', '.ППС2жИ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жР'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жД'),
+        WordForm(f'{pps2mi[:-2]}ую', '.ППС2жВ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жТ1'),
+        WordForm(f'{pps2mi[:-2]}ою', '.ППС2жТ2'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жП'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2сР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2сД'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2сТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2сП'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнИ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнР'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мнД'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнВ'),
+        WordForm(f'{pps2mi[:-2]}ыми', '.ППС2мнТ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнП'),
+        WordForm(f'{pps2mi[:-3]}', '.ППСКм2'),
+        WordForm(f'{pps2mi[:-3]}а', '.ППСКж2'),
+        WordForm(f'{pps2mi[:-3]}о', '.ППСКс2'),
+        WordForm(f'{pps2mi[:-3]}ы', '.ППСКмн2'),
+    ]
+
+    return word_forms
+
+
+# ППС3&6ж
+def passive_past_participle_pps3_6zh(src_dict) -> list:
+    name = src_dict['name']
+    pps1mi = f'{name[:-2]}тый'
+    pps2mi = f'{name[:-5]}женный'
+    word_forms = [
+        WordForm(pps1mi, '.ППС1мИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1мР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1мД'),
+        WordForm(pps1mi, '.ППС1мВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1мП'),
+        WordForm(f'{pps1mi[:-2]}ая', '.ППС1жИ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жР'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жД'),
+        WordForm(f'{pps1mi[:-2]}ую', '.ППС1жВ'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жТ1'),
+        WordForm(f'{pps1mi[:-2]}ою', '.ППС1жТ2'),
+        WordForm(f'{pps1mi[:-2]}ой', '.ППС1жП'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сИ'),
+        WordForm(f'{pps1mi[:-2]}ого', '.ППС1сР'),
+        WordForm(f'{pps1mi[:-2]}ому', '.ППС1сД'),
+        WordForm(f'{pps1mi[:-2]}ое', '.ППС1сВ'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1сТ'),
+        WordForm(f'{pps1mi[:-2]}ом', '.ППС1сП'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнИ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнР'),
+        WordForm(f'{pps1mi[:-2]}ым', '.ППС1мнД'),
+        WordForm(f'{pps1mi[:-2]}ые', '.ППС1мнВ'),
+        WordForm(f'{pps1mi[:-2]}ыми', '.ППС1мнТ'),
+        WordForm(f'{pps1mi[:-2]}ых', '.ППС1мнП'),
+        WordForm(f'{pps1mi[:-2]}', '.ППСКм1'),
+        WordForm(f'{pps1mi[:-2]}а', '.ППСКж1'),
+        WordForm(f'{pps1mi[:-2]}о', '.ППСКс1'),
+        WordForm(f'{pps1mi[:-2]}ы', '.ППСКмн1'),
+
+        WordForm(pps2mi, '.ППС2мИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2мР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2мД'),
+        WordForm(pps2mi, '.ППС2мВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2мП'),
+        WordForm(f'{pps2mi[:-2]}ая', '.ППС2жИ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жР'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жД'),
+        WordForm(f'{pps2mi[:-2]}ую', '.ППС2жВ'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жТ1'),
+        WordForm(f'{pps2mi[:-2]}ою', '.ППС2жТ2'),
+        WordForm(f'{pps2mi[:-2]}ой', '.ППС2жП'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сИ'),
+        WordForm(f'{pps2mi[:-2]}ого', '.ППС2сР'),
+        WordForm(f'{pps2mi[:-2]}ому', '.ППС2сД'),
+        WordForm(f'{pps2mi[:-2]}ое', '.ППС2сВ'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2сТ'),
+        WordForm(f'{pps2mi[:-2]}ом', '.ППС2сП'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнИ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнР'),
+        WordForm(f'{pps2mi[:-2]}ым', '.ППС2мнД'),
+        WordForm(f'{pps2mi[:-2]}ые', '.ППС2мнВ'),
+        WordForm(f'{pps2mi[:-2]}ыми', '.ППС2мнТ'),
+        WordForm(f'{pps2mi[:-2]}ых', '.ППС2мнП'),
+        WordForm(f'{pps2mi[:-3]}', '.ППСКм2'),
+        WordForm(f'{pps2mi[:-3]}а', '.ППСКж2'),
+        WordForm(f'{pps2mi[:-3]}о', '.ППСКс2'),
+        WordForm(f'{pps2mi[:-3]}ы', '.ППСКмн2'),
+    ]
+
+    return word_forms
