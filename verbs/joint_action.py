e8581d256c2df0e4dd091c05692546ca990f60ef
Aleksey Voko <voko.aleksey@gmail.com>
2021-05-06 11:00:31 +0400

    Added verbs joint_action С - С(3)

--- verbs/joint_action.py
+++ verbs/joint_action.py
@@ -8,6 +8,7 @@ def get_joint_action_forms(src_dict) -> list:
     joint_action_tmpl = {
         'С': joint_action_c,
         'С(2)': joint_action_c2,
+        'С(3)': joint_action_c3,
     }
     return joint_action_tmpl[src_dict['Inf_6']](src_dict)
 
@@ -50,5 +51,35 @@ def joint_action_c2(src_dict) -> list:
         word_forms = [
             WordForm(f'{gnb1mn1[:-2]}тесь', '.ГСДмн1'),
             WordForm(f'{gnb1mn2[:-2]}тесь', '.ГСДмн2'),
+        ]
+    return word_forms
+
+
+# С(3)
+def joint_action_c3(src_dict) -> list:
+    name = src_dict['name']
+    gnb1mn1 = list(filter(
+        lambda x: x.idf == '.ГНБ1мн1',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    gnb1mn2 = list(filter(
+        lambda x: x.idf == '.ГНБ1мн2',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    gnb1mn3 = list(filter(
+        lambda x: x.idf == '.ГНБ1мн3',
+        get_present_future_forms(src_dict)
+    ))[0].name
+    if not name.endswith(('ся', 'сь')):
+        word_forms = [
+            WordForm(f'{gnb1mn1}те', '.ГСДмн1'),
+            WordForm(f'{gnb1mn2}те', '.ГСДмн2'),
+            WordForm(f'{gnb1mn3}те', '.ГСДмн3'),
+        ]
+    else:
+        word_forms = [
+            WordForm(f'{gnb1mn1[:-2]}тесь', '.ГСДмн1'),
+            WordForm(f'{gnb1mn2[:-2]}тесь', '.ГСДмн2'),
+            WordForm(f'{gnb1mn3[:-2]}тесь', '.ГСДмн3'),
         ]
     return word_forms
