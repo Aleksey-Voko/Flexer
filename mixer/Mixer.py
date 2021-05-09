a3eb30c870841c5939b5f812fd1071bfe66287fe
Aleksey Voko <voko.aleksey@gmail.com>
2021-05-09 14:59:52 +0400

    Added icon to Mixer

--- mixer/Mixer.py
+++ mixer/Mixer.py
@@ -5,16 +5,14 @@ from utils import get_string_list_from_file, read_src_bs, save_bs_dicts_to_txt
 
 
 def add_groups_to_bs():
-    definitions = 'definitionsMixer.txt'
+    definitions = 'insertion.txt'
 
-    print('*' * 3)
     print('Настройки:')
     print(f'"{definitions}"\n')
     in_bs, *add_groups_list, out_bs = get_string_list_from_file(
         definitions, encoding='cp1251')
     add_groups_list = add_groups_list[1:-1]
 
-    print('*' * 3)
     print('База словоформ:')
     print(f'"{in_bs}"\n')
     word_forms_bases = list(read_src_bs(in_bs))
@@ -34,15 +32,12 @@ def add_groups_to_bs():
 
     save_bs_dicts_to_txt(sorted(word_forms_bases), out_bs)
 
-    print('*' * 3)
-    print(f'В БС добавлено {count} групп словоформ\n')
-
-    print('*' * 3)
-    print(f'Файл с результатами "{out_bs}" сохранён в текущей директории:')
+    print(f'В БС добавлено {count} групп словоформ')
+    print(f'Создан файл "{out_bs}"')
+    print(f'Файлы с результатами сохранены в текущей директории:')
     print(Path().resolve())
     print()
 
-    print('*' * 3)
     print('Для выхода нажмите любую Enter')
     input()
 
@@ -51,4 +46,4 @@ if __name__ == '__main__':
     add_groups_to_bs()
 
 # compilation
-# pyinstaller -F mixer/Mixer.py
+# pyinstaller -F -i mixer/icon.ico mixer/Mixer.py
