653546157c3fff87a4777af02e1925557e53920c
Aleksey Voko <voko.aleksey@gmail.com>
2021-05-10 20:07:08 +0400

    Added adjectives superlative_forms ПI1 - ПII8ш

--- adjectives/superlative_forms.py
+++ adjectives/superlative_forms.py
@@ -6,38 +6,354 @@ from word_form import WordForm
 def get_superlative_forms(src_dict) -> list:
     superlative_tmpl = {
         'ПI1': get_superlative_pi1,
+        'ПI2': get_superlative_pi2,
+        'ПII1': get_superlative_pii1,
+        'ПII2ж': get_superlative_pii2zh,
+        'ПII3ж': get_superlative_pii3zh,
+        'ПII4ч': get_superlative_pii4ch,
+        'ПII5ч': get_superlative_pii5ch,
+        'ПII6ч': get_superlative_pii6ch,
+        'ПII7ч': get_superlative_pii7ch,
+        'ПII8ш': get_superlative_pii8sh,
     }
     return superlative_tmpl[src_dict['Inf_3']](src_dict)
 
 
 # ПI1
 def get_superlative_pi1(src_dict) -> list:
-    name = src_dict['name']
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-2]}ейший'
     word_forms = [
-        WordForm(f'{name[:-2]}ейший', '.ППмИ'),
-        WordForm(f'{name[:-2]}ейшего', '.ППмР'),
-        WordForm(f'{name[:-2]}ейшему', '.ППмД'),
-        WordForm(f'{name[:-2]}ейший', '.ППмВ'),
-        WordForm(f'{name[:-2]}ейшим', '.ППмТ'),
-        WordForm(f'{name[:-2]}ейшем', '.ППмП'),
-        WordForm(f'{name[:-2]}ейшая', '.ППжИ'),
-        WordForm(f'{name[:-2]}ейшей', '.ППжР'),
-        WordForm(f'{name[:-2]}ейшей', '.ППжД'),
-        WordForm(f'{name[:-2]}ейшую', '.ППжВ'),
-        WordForm(f'{name[:-2]}ейшей', '.ППжТ1'),
-        WordForm(f'{name[:-2]}ейшею', '.ППжТ2'),
-        WordForm(f'{name[:-2]}ейшей', '.ППжП'),
-        WordForm(f'{name[:-2]}ейшее', '.ППсИ'),
-        WordForm(f'{name[:-2]}ейшего', '.ППсР'),
-        WordForm(f'{name[:-2]}ейшему', '.ППсД'),
-        WordForm(f'{name[:-2]}ейшее', '.ППсВ'),
-        WordForm(f'{name[:-2]}ейшим', '.ППсТ'),
-        WordForm(f'{name[:-2]}ейшем', '.ППсП'),
-        WordForm(f'{name[:-2]}ейшие', '.ППмнИ'),
-        WordForm(f'{name[:-2]}ейших', '.ППмнР'),
-        WordForm(f'{name[:-2]}ейшим', '.ППмнД'),
-        WordForm(f'{name[:-2]}ейшие', '.ППмнВ'),
-        WordForm(f'{name[:-2]}ейшими', '.ППмнТ'),
-        WordForm(f'{name[:-2]}ейших', '.ППмнП'),
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПI2
+def get_superlative_pi2(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-3]}ейший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII1
+def get_superlative_pii1(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-2]}айший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII2ж
+def get_superlative_pii2zh(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-3]}жайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII3ж
+def get_superlative_pii3zh(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-4]}жайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII4ч
+def get_superlative_pii4ch(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-3]}чайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII5ч
+def get_superlative_pii5ch(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-4]}чайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII6ч
+def get_superlative_pii6ch(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-3]}ьчайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII7ч
+def get_superlative_pii7ch(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-4]}гчайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
+    ]
+    return word_forms
+
+
+# ПII8ш
+def get_superlative_pii8sh(src_dict) -> list:
+    pmi = src_dict['name']
+    ppmi = f'{pmi[:-3]}шайший'
+    word_forms = [
+        WordForm(ppmi, '.ППмИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППмР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППмД'),
+        WordForm(ppmi, '.ППмВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППмП'),
+        WordForm(f'{ppmi[:-2]}ая', '.ППжИ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжР'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжД'),
+        WordForm(f'{ppmi[:-2]}ую', '.ППжВ'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжТ1'),
+        WordForm(f'{ppmi[:-2]}ею', '.ППжТ2'),
+        WordForm(f'{ppmi[:-2]}ей', '.ППжП'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсИ'),
+        WordForm(f'{ppmi[:-2]}его', '.ППсР'),
+        WordForm(f'{ppmi[:-2]}ему', '.ППсД'),
+        WordForm(f'{ppmi[:-2]}ее', '.ППсВ'),
+        WordForm(f'{ppmi[:-2]}им', '.ППсТ'),
+        WordForm(f'{ppmi[:-2]}ем', '.ППсП'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнИ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнР'),
+        WordForm(f'{ppmi[:-2]}им', '.ППмнД'),
+        WordForm(f'{ppmi[:-2]}ие', '.ППмнВ'),
+        WordForm(f'{ppmi[:-2]}ими', '.ППмнТ'),
+        WordForm(f'{ppmi[:-2]}их', '.ППмнП'),
     ]
     return word_forms
