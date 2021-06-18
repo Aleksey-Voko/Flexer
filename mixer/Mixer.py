ea85cb27a878f443d4c46eb36bb54803d2b36f0d
Aleksey Voko <voko.aleksey@gmail.com>
2021-06-18 19:40:19 +0400

    Added InputDataError to Mixer

--- mixer/Mixer.py
+++ mixer/Mixer.py
@@ -17,6 +17,7 @@ def add_groups_to_bs():
     print(f'"{in_bs}"\n')
     print(f'... чтение "{in_bs}" ...\n')
     word_forms_bases = list(read_src_bs(in_bs))
+    word_form_names = [x.title_word_form.name for x in word_forms_bases]
 
     print('Файлы с группами:')
     pprint(add_groups_list)
@@ -26,6 +27,32 @@ def add_groups_to_bs():
 
     for add_groups in add_groups_list:
         verbs = list(read_src_bs(add_groups))
+
+        for group_word_form in verbs:
+            title_form = group_word_form.title_word_form
+            name_title = title_form.name
+
+            if not name_title.islower():
+                print('В Н И М А Н И Е !')
+                print('В добавляемых ЗС групп или одиночках найдены заглавные буквы:')
+                print(title_form)
+                print('Для продолжения нажмите Enter')
+                input()
+
+            if 'ё' in name_title:
+                print('В Н И М А Н И Е !')
+                print('В добавляемых ЗС групп или одиночках найдены буквы "Ёё":')
+                print(title_form)
+                print('Для продолжения нажмите Enter')
+                input()
+
+            if name_title in word_form_names:
+                print('В Н И М А Н И Е !')
+                print('Новые случаи омонимии!!!')
+                print(title_form)
+                print('Для продолжения нажмите Enter')
+                input()
+
         word_forms_bases += verbs
         print(f'Дабавлены данные из файла "{add_groups}"')
         count += len(verbs)
