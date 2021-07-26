08ced33d85bd651553d4d15c29223dffc3c63149
Aleksey Voko <voko.aleksey@gmail.com>
2021-07-26 17:14:30 +0400

    Fixed Mixer

--- mixer/Mixer.py
+++ mixer/Mixer.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from pprint import pprint
 
 from utils import get_string_list_from_file, read_src_bs, save_bs_dicts_to_txt
 
@@ -20,7 +19,8 @@ def add_groups_to_bs():
     word_form_names = [x.title_word_form.name for x in word_forms_bases]
 
     print('Файлы с добавляемыми группами словоформ:')
-    pprint(add_groups_list)
+    for line in add_groups_list:
+        print(line)
     print()
 
     count = 0
@@ -54,60 +54,57 @@ def add_groups_to_bs():
         word_forms_bases += verbs
         count += len(verbs)
 
-    if any([capital_letters, invalid_characters, added_homonyms]):
-        print(f'{"* " * 38}*\n')
+    print(f'{"* " * 38}*\n')
 
     if capital_letters:
         print('В Н И М А Н И Е !')
         print('В добавляемых ЗС групп и одиночках найдены заглавные буквы:\n')
-        pprint(capital_letters)
+        for line in capital_letters:
+            print(line)
         print('\nДля продолжения нажмите Enter')
+        input()
+    else:
+        print('В Н И М А Н И Е !')
+        print('В добавляемых ЗС групп и одиночках НЕТ заглавных букв.')
+        print('Для продолжения нажмите Enter')
         input()
 
     if invalid_characters:
         print('В Н И М А Н И Е !')
         print('В добавляемых ЗС групп и одиночках найдена буква Ёё:\n')
-        pprint(invalid_characters)
+        for line in invalid_characters:
+            print(line)
         print('\nДля продолжения нажмите Enter')
+        input()
+    else:
+        print('В Н И М А Н И Е !')
+        print('В добавляемых ЗС групп и одиночках НЕТ буквы Ёё.')
+        print('Для продолжения нажмите Enter')
         input()
 
     if added_homonyms:
         print('В Н И М А Н И Е !')
         print('Новые случаи омонимии!!!')
-        print('В БС среди ЗС групп и одиночек ОБНАРУЖЕНЫ слова, омонимичные добавляемым ЗС групп и одиночкам')
-        print('Добавляемые:')
-        pprint(added_homonyms)
-        pprint(sorted(list(set(existing_homonyms))))
-        print('Уже имеющиеся в БС:')
-        pprint(sorted(list(set(existing_homonyms))))
+        print('В БС среди ЗС групп и одиночек ОБНАРУЖЕНЫ слова, омонимичные добавляемым ЗС групп и одиночкам.')
+        print('\nДобавляемые:')
+        for line in sorted(list(set(added_homonyms))):
+            print(line)
+        print('\nУже имеющиеся в БС:')
+        for line in sorted(list(set(existing_homonyms))):
+            print(line)
         print('Для продолжения нажмите Enter')
         input()
-
-    if any([capital_letters, invalid_characters, added_homonyms]):
-        print('Необходимо исправить ошибки входных данных')
-        print('Для выхода нажмите Enter')
-        input()
-        quit()
     else:
         print('В Н И М А Н И Е !')
-        print('В добавляемых ЗС групп и одиночках НЕТ заглавных букв')
+        print('Новых случаев омонимии НЕТ.')
+        print('В БС среди ЗС групп и одиночек НЕ обнаружено слов, омонимичных добавляемым ЗС групп и одиночкам.')
         print('Для продолжения нажмите Enter')
         input()
 
-        print('В Н И М А Н И Е !')
-        print('В добавляемых ЗС групп и одиночках НЕТ буквы Ёё')
-        print('Для продолжения нажмите Enter')
-        input()
-
-        print('В Н И М А Н И Е !')
-        print('Новых случаев омонимии НЕТ')
-        print('В БС среди ЗС групп и одиночек НЕ обнаружено слов, омонимичных добавляемым ЗС групп и одиночкам')
-        print('Для продолжения нажмите Enter')
-        input()
-
-        print('Добавлены данные из файлов:')
-        print(f'{add_groups_list}')
-        print()
+    print('Добавлены данные из файлов:')
+    for line in add_groups_list:
+        print(line)
+    print()
 
     print('... сортировка ...\n')
     save_bs_dicts_to_txt(sorted(word_forms_bases), out_bs)
