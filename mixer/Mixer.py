d6477b23e51ee93a832e09b275fc7b4cb3530e01
Aleksey Voko <voko.aleksey@gmail.com>
2021-07-31 19:56:39 +0400

    Fixed Mixer

--- mixer/Mixer.py
+++ mixer/Mixer.py
@@ -6,15 +6,15 @@ from utils import get_string_list_from_file, read_src_bs, save_bs_dicts_to_txt
 def add_groups_to_bs():
     definitions = 'insertion.txt'
     print('Настройки:')
-    print(f'"{definitions}"\n')
+    print(f'{definitions}\n')
 
     in_bs, *add_groups_list, out_bs = get_string_list_from_file(
         definitions, encoding='cp1251')
     add_groups_list = add_groups_list[1:-1]
 
     print('База словоформ:')
-    print(f'"{in_bs}"\n')
-    print(f'... чтение "{in_bs}" ...\n')
+    print(f'{in_bs}\n')
+    print(f'... чтение {in_bs} ...\n')
     word_forms_bases = list(read_src_bs(in_bs))
     word_form_names = [x.title_word_form.name for x in word_forms_bases]
 
@@ -112,7 +112,7 @@ def add_groups_to_bs():
     save_bs_dicts_to_txt(sorted(word_forms_bases), out_bs)
 
     print(f'В БС добавлено {count} групп словоформ')
-    print(f'Создан файл "{out_bs}"\n')
+    print(f'Создан файл {out_bs}\n')
     print(f'Файлы с результатами сохранены в текущей директории:')
     print(Path().resolve())
     print()
