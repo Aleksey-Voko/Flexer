aa4d860a518f3f3aaea3fc1abf17319d5dab7dae
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-16 19:44:13 +0400

    Added task BSLists -> 'Существительные мн. ч.txt'

--- bs_lists/nouns.py
+++ bs_lists/nouns.py
@@ -68,3 +68,19 @@ def get_non_plural_nouns(word_forms_bases, _) -> list:
             group.title_word_form.info))
     ]
     return word_forms
+
+
+# Существительные мн. ч.txt
+def get_plural_nouns(word_forms_bases, _) -> list:
+    """Найти в БС строки с ЗС групп, идентификатор которых содержит .С ,
+    и в спец. информации имеется индикатор мн. ч. мн
+    или индикатор мн. ч. с индикатором "!": мн! ."""
+
+    word_forms = [
+        str(group.title_word_form) for group in word_forms_bases
+        if group.title_word_form.idf.startswith('.С')
+           and any(map(
+            lambda x: x.startswith('мн'),
+            group.title_word_form.info))
+    ]
+    return word_forms
