07b8fbef222a2ca7f2dfdc17ea18c0e6afc97e05
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-19 10:12:16 +0400

    Added task BSLists -> 'Прилагательные. Нет ед. ч.txt'

--- bs_lists/adjectives.py
+++ bs_lists/adjectives.py
@@ -36,3 +36,19 @@ def get_non_plural_adjectives(word_forms_bases, _) -> list:
            and 'е' in group.title_word_form.info
     ]
     return word_forms
+
+
+# Прилагательные. Нет ед. ч.txt
+def get_non_singular_adjectives(word_forms_bases, _) -> list:
+    """Найти в БС строки с ЗС групп, идентификатор которых содержит .П ,
+    и в спец. информации указан шаблон полной формы,
+    название которого содержит мн ."""
+
+    word_forms = [
+        str(group.title_word_form) for group in word_forms_bases
+        if group.title_word_form.idf.startswith('.П')
+           and any(map(
+            lambda x: 'мн' in x,
+            group.title_word_form.info))
+    ]
+    return word_forms
