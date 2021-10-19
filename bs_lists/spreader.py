f75133ab8d700a201ce4d0811c3c65319639b00b
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-19 09:53:02 +0400

    Added task BSLists -> 'Прилагательные. Нет мн. ч.txt'

--- bs_lists/spreader.py
+++ bs_lists/spreader.py
@@ -1,4 +1,5 @@
-from bs_lists.adjectives import get_adjectives, get_animate_adjectives
+from bs_lists.adjectives import (get_adjectives, get_animate_adjectives,
+                                 get_non_plural_adjectives)
 from bs_lists.nouns import (get_nouns, get_inanimate_nouns, get_animate_nouns,
                             get_singular_nouns, get_non_plural_nouns,
                             get_plural_nouns, get_non_singular_nouns,
@@ -78,6 +79,7 @@ EXPLICIT_TASKS = {
 
     'Прилагательные.txt': get_adjectives,
     'Прилагательные одуш.txt': get_animate_adjectives,
+    'Прилагательные. Нет мн. ч.txt': get_non_plural_adjectives,
 
     'Местоимения.txt': get_pronouns,
     'Местоимения. Нет мн. ч.txt': get_pronouns_no_plural,
