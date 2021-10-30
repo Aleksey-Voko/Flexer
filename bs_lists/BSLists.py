3ef4de1997d9a4d8eabf86f54c93e51326dc8f11
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-30 15:09:46 +0400

    fix(bs_lists): fix task file name '*/\|'

--- bs_lists/BSLists.py
+++ bs_lists/BSLists.py
@@ -49,10 +49,9 @@ def main():
             out_task = get_filtered_list(word_forms_bases, socket_group_list,
                                          task)
             if out_task:
-                task = task.replace('*', '+')
+                task = task.replace('*', '^')
                 task = task.replace('/', '[')
-                task = task.replace('\\', ']')
-                task = task.replace('|', 'l')
+                task = task.replace('|', ']')
                 save_list_to_file(out_task, task, encoding='cp1251')
                 print(f'Создан документ: {task}\n')
                 print('Для продолжения нажмите Enter')
