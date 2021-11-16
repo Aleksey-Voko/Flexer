c85c4a6f04dcfdd1894c5ccebab2db903c3cef9f
Aleksey Voko <voko.aleksey@gmail.com>
2021-11-16 18:26:22 +0400

    feature(bs_lists): add function for 'Безличные глаголы.txt'.

--- bs_lists/spreader.py
+++ bs_lists/spreader.py
@@ -79,7 +79,8 @@ from bs_lists.socket_bg import (save_multi_root_words,
                                 get_ordinary_words_bg, get_bg_snapshot)
 from bs_lists.verbs import (get_verbs, get_imperfective_verbs,
                             get_perfect_verbs, get_two_species_verbs,
-                            get_transitive_verbs, get_intransitive_verbs)
+                            get_transitive_verbs, get_intransitive_verbs,
+                            get_impersonal_verbs)
 
 EXPLICIT_TASKS = {
     # БС
@@ -139,6 +140,7 @@ EXPLICIT_TASKS = {
     'Двувидовые глаголы.txt': get_two_species_verbs,
     'Переходные глаголы.txt': get_transitive_verbs,
     'Непереходные глаголы.txt': get_intransitive_verbs,
+    'Безличные глаголы.txt': get_impersonal_verbs,
 
     'Местоимения.txt': get_pronouns,
     'Местоимения. Нет мн. ч.txt': get_pronouns_no_plural,
--- bs_lists/verbs.py
+++ bs_lists/verbs.py
@@ -75,3 +75,16 @@ def get_intransitive_verbs(word_forms_bases, _) -> list:
            and 'неп' in group.title_word_form.info
     ]
     return word_forms
+
+
+# Безличные глаголы.txt
+def get_impersonal_verbs(word_forms_bases, _) -> list:
+    """Найти в БС строки с ЗС групп, идентификатор которых содержит .Г ,
+    и в спец. информации имеется индикатор безличности б ."""
+
+    word_forms = [
+        str(group.title_word_form) for group in word_forms_bases
+        if group.title_word_form.idf.startswith('.Г')
+           and 'б' in group.title_word_form.info
+    ]
+    return word_forms
