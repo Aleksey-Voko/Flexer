9a7c1c9b2ed7fd6b2ccd1b9cf0df2a44720d8fe0
Aleksey Voko <voko.aleksey@gmail.com>
2021-11-23 22:06:33 +0400

    fix(bs_lists): fix function for 'Прилагательные. Идентификаторы.txt'

--- bs_lists/adjectives.py
+++ bs_lists/adjectives.py
@@ -650,7 +650,7 @@ def get_adjectives_identifiers(word_forms_bases, _) -> list:
     identifiers += sorted(list(pzt_idf))
     identifiers += sorted(list(pzp_idf))
 
-    identifiers += sorted(list(pzi_idf))
+    identifiers += sorted(list(psi_idf))
     identifiers += sorted(list(psr_idf))
     identifiers += sorted(list(psd_idf))
     identifiers += sorted(list(psv_idf))
@@ -685,7 +685,7 @@ def get_adjectives_identifiers(word_forms_bases, _) -> list:
     identifiers += sorted(list(ppzt_idf))
     identifiers += sorted(list(ppzp_idf))
 
-    identifiers += sorted(list(ppzi_idf))
+    identifiers += sorted(list(ppsi_idf))
     identifiers += sorted(list(ppsr_idf))
     identifiers += sorted(list(ppsd_idf))
     identifiers += sorted(list(ppsv_idf))
